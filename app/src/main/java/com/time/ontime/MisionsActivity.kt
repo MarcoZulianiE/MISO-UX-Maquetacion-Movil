1e782be86e51e81fef47f2b7696058156abeba66
Daniel Bernal <da.bernalc1@uniandes.edu.co>
2023-09-27 03:48:57 -0500

    All Activities created

---
+++ app/src/main/java/com/time/ontime/MisionsActivity.kt
@@ -0,0 +1,11 @@
+package com.time.ontime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MisionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_misions)
+    }
+}
